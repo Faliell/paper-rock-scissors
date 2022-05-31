821d1c8da4152d2c20af9c41a250986019cccfc2
Faliell <76968922+Faliell@users.noreply.github.com>
2022-05-31 20:40:12 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,72 @@
+let playerPoint = 0;
+let cpuPoint = 0;
+
+function computerPlay() {
+  let randomNumber = Math.floor(Math.random() * 3);
+  if (randomNumber === 0) {
+    return "rock";
+  }
+  if (randomNumber === 1) {
+    return "paper";
+  }
+  if (randomNumber === 2) {
+    return "scissors";
+  }
+}
+
+function singleRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return "Draw";
+  } else if (playerSelection === "rock") {
+    if (computerSelection === "paper") {
+      cpuPoint++;
+      return "You lose, Paper beats Rock";
+    } else {
+      playerPoint++;
+      return "You win, Rock beats Sissors";
+    }
+  } else if (playerSelection === "paper") {
+    if (computerSelection === "rock") {
+      playerPoint++;
+      return "You win, Paper beats Rock";
+    } else {
+      cpuPoint++;
+      return "You lose, Sissors beats Paper";
+    }
+  } else if (playerSelection === "sissors") {
+    if (computerSelection === "paper") {
+      playerPoint++;
+      return "You win, Sissors beats Paper";
+    } else {
+      cpuPoint++;
+      return "You lose, Rock beats Sissors";
+    }
+  } else {
+    return "Error Typo";
+  }
+}
+
+function game() {
+  for (let i = 0; i < 5; i++) {
+    const playerSelection = window
+      .prompt("Rock, Sissors or Paper?")
+      .toLowerCase();
+    const computerSelection = computerPlay();
+
+    console.log(
+      `${singleRound(
+        playerSelection,
+        computerSelection
+      )} \nplayer:${playerPoint} \nCPU: ${cpuPoint}`
+    );
+  }
+  if (playerPoint === cpuPoint) {
+    console.log("Draw");
+  } else if (playerPoint > cpuPoint) {
+    console.log("You Win the Game!");
+  } else {
+    console.log("You Lose the Game!");
+  }
+}
+
+game();
